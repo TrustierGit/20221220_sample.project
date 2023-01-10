cca939bd3ee2021020cff8196b09b0f6484af4a6
株式会社TRUSTIER <87689062+TrustierGit@users.noreply.github.com>
2023-01-10 09:25:40 +0900

    error pages

---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,16 @@
+<x-guest-layout>
+    <section class="text-gray-600 body-font relative">
+        <div class="container px-5 py-24 mx-auto">
+            <div class="flex flex-col text-center mb-12">
+                <div id ="error_title" class="font-bold text-4xl py-12">404 Page Not Fund</div>
+
+                <div class="font-bold text-2xl p-12">アクセスしようとしたページが見つかりませんでした。
+                </div>
+                <div class="p-2 w-full">
+                    <button class="flex mx-auto text-white bg-indigo-500 border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded text-lg" onclick="location.href='{{ route('login') }}' ">トップページに戻る</button>
+                </div>
+            </div>
+        </div>
+
+    </section>
+</x-guest-layout>
