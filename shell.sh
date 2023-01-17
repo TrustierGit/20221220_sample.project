79ec279cca50c64604a7645f6715c95ca6c7e9fc
株式会社TRUSTIER <87689062+TrustierGit@users.noreply.github.com>
2023-01-17 13:57:31 +0900

    Update shell.sh

--- shell.sh
+++ shell.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
-
-while read TOKEN
-do
-
-curl -H "Authorization: Bearer $TOKEN" http://127.0.0.1:8000/api/MakeFileAPI
-
-
-done < 'C:\Users\USER\Desktop\out.txt'
-
+CURRENT_DIR=$(cd $(dirname $0);pwd)
+LOG_DIR=$CURRENT_DIR/log
+mkdir -p $LOG_DIR
+LOG_FILE=$LOG_DIR/$(date +"%Y%m%d").log
+touch $LOG_FILE
+echo "-- START --" >> $LOG_FILE
+echo $(date +"%Y-%m-%d %H:%M:%S") >> $LOG_FILE
+TOKEN=$(mysql --defaults-extra-file=sql.conf -N  -e 'SELECT remember_token FROM reservation_db.users WHERE `id`=1 ;')
+curl -H "Authorization: Bearer $TOKEN" http://127.0.0.1:80/api/MakeFileAPI >> $LOG_FILE
+echo $(date +"%Y-%m-%d %H:%M:%S") >> $LOG_FILE
+echo "-- END --" >> $LOG_FILE
