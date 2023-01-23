f051efcbeaff18b8e7b7237e317f41908dca1386
株式会社TRUSTIER <87689062+TrustierGit@users.noreply.github.com>
2023-01-23 09:54:36 +0900

    delete comment

--- resources/views/download.blade.php
+++ resources/views/download.blade.php
@@ -2,9 +2,7 @@
 <div class="container px-5 py-4 mx-auto">
   <h1 class="text-2xl font-medium title-font text-gray-900 py-12 ">■予約履歴ダウンロード</h1>
         <div class="h-full w-3/4 m-auto p-8 rounded bg-white border-2 border-gray-400">
-            <!-- <span class="flex-grow flex flex-col pl-4"> -->
             <div class="flex-grow flex flex-col pl-4">
-              <!-- <span class="title-font font-medium text-gray-900"> -->
               <div class="title-font font-medium text-gray-900">
               <section class="text-gray-600 body-font">
                 
